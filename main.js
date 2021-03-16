33ccb6829e505a28b9665768cab14f6b83342f65
chrislemus <icrislemus@gmail.com>
2021-03-16 14:49:48 -0400

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,58 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.querySelector('#modal')
+  // const mediaPosts = document.querySelectorAll('.media-post')
+  const PostsLikeBtns = document.querySelectorAll('.like')
 
+  modal.hidden = true
+
+  // mimicServerCall()
+  // .then(res => res.json())
+  // .then(likes => likes.map(like => like.postId))
+  // .then(postIds => postIds.forEach(id => {
+  //   const post = document.getElementById(id)
+  //   if(post) updateHeart(post);
+  // }))
+
+  function updateHeart(heartParentNode) {
+    const likeHeart = heartParentNode.querySelector('.like-glyph')
+    const hasLike = likeHeart.classList.contains('activated-heart')
+    if (hasLike) {
+      likeHeart.classList.remove('activated-heart')
+    } else {
+      likeHeart.classList.add('activated-heart')
+    }
+    likeHeart.innerHTML = hasLike ? EMPTY_HEART : FULL_HEART
+  }
+
+  
+  PostsLikeBtns.forEach(likeBtn => {
+
+    // likeBtn.classList.add('activated-heart')
+
+
+    likeBtn.addEventListener('click', e => {
+      const postId = e.target.closest('.media-post').id
+      const config = {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        // body: JSON.stringify({postId})
+      }
+      mimicServerCall("http://mimicServer.example.com", config)
+      .then(res => updateHeart(likeBtn))
+      .catch(err => {
+        modal.hidden = false
+        modal.querySelector('#modal-message').innerHTML = err
+        setTimeout(() => {
+          modal.hidden = true
+        }, 5000)
+      })
+    })
+  })
+  
+})
 
 
 
